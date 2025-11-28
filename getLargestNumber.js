2034e863866c99b1d8bb35dee6d911432211c718
Asad-Ali-Developer <uhope1645@gmail.com>
2025-11-28 23:46:32 +0500

    Practices of getSquareOfEvenNumbers

--- getLargestNumber.js
+++ getLargestNumber.js
@@ -1,17 +1,15 @@
 let arr = [1, 2, 3, 4, 5, 5, 6, 7, 7, 7, 8, 9];
 
 const getLargestNumber = (arr) => {
-  let large = 0;
-  let largeNumIndex = 0
+  let largeNum = 0;
 
-  for(let i = 1; i < arr.length; i++){
-    if(arr[i] < large){
-        large = arr[i];
-        largeNumIndex = i
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] > largeNum) {
+      largeNum = arr[i];
     }
   }
 
-  return {largeNum: large, largeNumIndex: largeNumIndex};
-};
+  return largeNum;
+}
 
 console.log(getLargestNumber(arr));
---
+++ getSquaresOfEvenNumbers.js
@@ -0,0 +1,33 @@
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// ----------- (Method 1) Using flatMap -----------
+
+// const squaredEvenNumbers = arr.flatMap((num) => {
+//   return num % 2 === 0 ? num * num : [];
+// });
+
+// console.log(squaredEvenNumbers);
+
+// ----------- (Method 2) Using for...of loop -----------
+
+const getSquaresOfEvenNumbers = (arr) => {
+  let squaredEvenNumbers = [];
+
+  for (let num of arr) {
+    if (num % 2 === 0) {
+      squaredEvenNumbers.push(num * num);
+    }
+  }
+
+  return squaredEvenNumbers;
+};
+
+console.log(getSquaresOfEvenNumbers(arr));
+
+//  ------------ (Method 3) Using the map() and filter() methods -----------
+
+// const squaredEvenNumbers = arr
+//   .filter((num) => num % 2 === 0)
+//   .map((evenNum) => evenNum * evenNum);
+
+// console.log(squaredEvenNumbers);
