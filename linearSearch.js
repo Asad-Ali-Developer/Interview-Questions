af6aa68828a7bd3deda860f41a29e9d8b3bbd9c4
Asad-Ali-Developer <uhope1645@gmail.com>
2026-02-11 20:06:42 +0500

    Feb 11 Updates

--- linearSearch.js
+++ linearSearch.js
@@ -1,17 +1,40 @@
-let arr = [1, 2, 3, 4, 5];
+let arr = [1, 2, 3, 4, 5, 6, 7];
+
+// const linearSearch = (arr, target) => {
+//   let searchedFor;
+//   let searchedForIndex;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === target) {
+//       searchedFor = arr[i];
+//       searchedForIndex = i;
+//     }
+//   }
+
+//   return { searchedFor, searchedForIndex };
+// };
 
 const linearSearch = (arr, target) => {
   let searchedFor;
-  let searchedForIndex;
+  let indexOfSearchedFor;
+
+  // for(let num of arr) {
+  //   if(num === target) {
+  //     searchedFor = num
+  //     indexOfSearchedFor = arr[num]
+  //   }
+  // }
 
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === target) {
-      searchedFor = arr[i];
-      searchedForIndex = i;
+    let num = arr[i];
+
+    if (num === target) {
+      searchedFor = num;
+      indexOfSearchedFor = i;
     }
   }
 
-  return { searchedFor, searchedForIndex };
+  return { searchedFor, indexOfSearchedFor };
 };
 
-console.log(linearSearch(arr, 5));
+console.log(linearSearch(arr, 7));
